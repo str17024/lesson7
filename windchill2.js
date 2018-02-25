3641f301825830cef5f9c9009c3c6565eb301f90
Ken Strobell <35313618+str17024@users.noreply.github.com>
2018-02-25 12:03:58 -0700

    Update windchill2.js

--- windchill2.js
+++ windchill2.js
@@ -1,11 +1,15 @@
+
+
 function computeNum() {
 	// Input - get input and validate if correct length and if a number
-	var textInput = get("aNum").value;
+	var textInput = 89;
 	if (textInput.length < 3 || isNaN(textInput)) return; // Validation return nothing
 	
 	// Processing - multiply by the a random number taken to the 3rd power
+	var ta = 75
+	var v = 3
 	var theNumber = parseInt(textInput); // OK - parse string to number
-	var theValue = theNumber * Math.pow(Math.random(), 3);
+	var theValue = theNumber * 35.74 + (0.6215 * ta) - 35.75 *Math.pow(v,0.16) + 0.4275 * ta * Math.pow(v, 0.16);
 	
 	// Output - output to the div and round the number value to two decimal places
 	get("output").innerHTML = "The value is <strong>" + theValue.toFixed(2) + "</strong>!";
